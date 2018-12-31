0f5e870549ec96ee50524817bc515470318d31a6
gorbondiga <gorbondiga@gmail.com>
2018-12-31 12:31:57 +0100

    vim conqueterm shells added

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 ###################################
 # tmux configuration
 ###################################
@@ -8,60 +7,33 @@
 if [ $(dpkg-query -W -f tmux 2>/dev/null | grep -c "ok installed") != 0 ]; then
     apt install tmux
 fi
-
 # clone tmux-resurrect
 if [ ! -d ~/tmux ]; then
     mkdir ~/tmux;
     git clone https://github.com/tmux-plugins/tmux-resurrect ~/tmux;
 fi
-
 # add tmux configuration, resurrect and xterm-keys
-if [ ! -f ~/.tmux.conf ]; then
-    touch ~/.tmux.conf;
-    echo "set-window-option -g xterm-keys on" >> ~/.tmux.conf;
-    # Move panes with mouse
-    # echo "set-option -g mouse-select-pane on" >> ~/.tmux.conf;
-    # Move panes with Alt+arrow
-    echo "bind -n M-Left select-pane -L" >> ~/.tmux.conf;   
-    echo "bind -n M-Right select-pane -R" >> ~/.tmux.conf;
-    echo "bind -n M-Up select-pane -U" >> ~/.tmux.conf;
-    echo "bind -n M-Down select-pane -D" >> ~/.tmux.conf;
-    # install tmux-resurrect
-    echo "set -g @plugin 'tmux-plugins/tmux-resurrect'" >> ~/.tmux.conf;
-    echo "set -g @plugin 'tmux-plugins/tmux-continuum'" >> ~/.tmux.conf;
-    echo "set -g @resurrect-processes 'ssh psql mysql sqlite3'" >> ~/.tmux.conf;
-    echo "run-shell ~/tmux/resurrect.tmux" >> ~/.tmux.conf;
-fi
+cp tmux.conf ~/.tmux.conf;
 
 ###################################
 # vim configuration
 ###################################
-
 # ensure the .vim directory exists
 if [ ! -d ~/.vim ]; then
     mkdir ~/.vim;
     touch ~/.vim/packages.vim;
     touch ~/.vim/shortcuts.vim;
 fi
-
-# setup powerline fonts
 # download Vundle files
 if [ ! -d ~/.vim/bundle ]; then
-#    git clone git@github.com:powerline/fonts.git ~/.powerline_fonts
-#    . ~/.powerline_fonts/install.sh
-#    sudo -E fc-cache -vf ~/.fonts/
     git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim;
 fi
 # copy preconfigured vimrc
+cp ~/dotfile/vimrc ~/.vimrc;
 if [ ! -f ~/.vimrc ]; then
-    cp ~/dotfile/vimrc ~/.vimrc;
     vim +PluginInstall +qall;
     cd ~/.vim/bundle/YouCompleteMe;
     ./install.sh --clang-completer;
-    echo "set tabstop=4" >> ~/.vimrc;
-    echo 'syntax enable " light' >> ~/.vimrc;
-    echo "set background=dark" >> ~/.vimrc; 
-    echo "colorscheme atomified" >> ~/.vimrc;
 fi
 # add colors to vim
 if [ ! -d ~/.vim/colors ]; then
--- vimrc
+++ vimrc
@@ -1,5 +1,10 @@
 set nocompatible              " be iMproved, required
+set nu
+set tabstop=4
+
 filetype off                  " required
+
+let mapleader = "<"
 
 " set the runtime path to include Vundle and initialize
 set rtp+=~/.vim/bundle/Vundle.vim
@@ -17,18 +22,33 @@ Plugin 'scrooloose/nerdtree'
 Plugin 'puppetlabs/puppet-syntax-vim'
 Plugin 'tpope/vim-fugitive'
 Plugin 'rstacruz/sparkup', {'rtp': 'vim/'}
+Plugin 'wkentaro-archive/conque.vim'
+Plugin 'fatih/vim-go'
+" Plugin 'christoomey/vim-tmux-navigator'
 
 " All of your Plugins must be added before the following line
 call vundle#end()            " required
 filetype plugin indent on    " required
-" To ignore plugin indent changes, instead use:
-"filetype plugin on
-"
-" Brief help
-" :PluginList       - lists configured plugins
-" :PluginInstall    - installs plugins; append `!` to update or just :PluginUpdate
-" :PluginSearch foo - searches for foo; append `!` to refresh local cache
-" :PluginClean      - confirms removal of unused plugins; append `!` to auto-approve removal
-"
-" see :h vundle for more details or wiki for FAQ
-" Put your non-Plugin stuff after this line
+
+" Mapped keys to send code lines to Conque-Shell
+imap <silent> <leader>j <Esc>Vy<C-w>jp
+nmap <silent> <leader>j Vy<C-w>jp
+vmap <silent> <leader>j y<C-w>jp
+
+command IPython ConqueTermSplit ipython
+command Bash ConqueTermSplit bash
+command Python ConqueTermSplit python
+
+set tabstop=4
+set background=dark
+syntax enable light
+colorscheme atomified
+
+let g:NERDTreeQuitOnOpen = 1
+let g:NERDTreeWinSize = 20
+" autocmd VimEnter * NERDTree
+
+let g:ConqueTerm_PyVersion = 3
+let g:ConqueTerm_FastMode = 1
+let g:ConqueTerm_CWInsert = 1
+" let pyshell = conque_term#open('ipython', ['split', 'resize 20'], 0)
